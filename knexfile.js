11d63134d7c9c6500dc3a8f124b99f3b2c050cd6
Billy <14883302+billyadam@users.noreply.github.com>
2026-05-02 15:13:35 +0700

    add knex

---
+++ knexfile.js
@@ -0,0 +1,31 @@
+require('dotenv').config();
+
+/** @type {import('knex').Knex.Config} */
+const base = {
+  client: 'pg',
+  connection: {
+    host: process.env.DB_HOST || 'localhost',
+    port: process.env.DB_PORT || 5432,
+    database: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+  },
+  migrations: {
+    directory: './src/database/migrations',
+    tableName: 'knex_migrations',
+  },
+  seeds: {
+    directory: './src/database/seeds',
+  },
+};
+
+module.exports = {
+  development: base,
+  production: {
+    ...base,
+    connection: {
+      ...base.connection,
+      ssl: { rejectUnauthorized: false },
+    },
+  },
+};
