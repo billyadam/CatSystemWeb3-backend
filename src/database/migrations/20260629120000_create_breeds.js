f83e440860de265fe628987043038ee1c606329f
Billy <14883302+billyadam@users.noreply.github.com>
2026-06-29 21:03:35 +0700

    change desc to name long

--- src/database/migrations/20260629120000_create_breeds.js
+++ src/database/migrations/20260629120000_create_breeds.js
@@ -5,7 +5,7 @@ exports.up = function (knex) {
   return knex.schema.createTable('breeds', (table) => {
     table.increments('id').primary();
     table.string('name', 64).notNullable().unique();
-    table.string('description', 512).notNullable().defaultTo('');
+    table.string('name_long', 512).notNullable().defaultTo('');
     table.timestamps(true, true);
   });
 };
--- src/repositories/breedRepository.js
+++ src/repositories/breedRepository.js
@@ -1,7 +1,7 @@
 const db = require('../database/knex');
 
 async function getAllBreeds() {
-  return db('breeds').select('id', 'name', 'description').orderBy('name');
+  return db('breeds').select('id', 'name', 'name_long').orderBy('name');
 }
 
 module.exports = { getAllBreeds };
